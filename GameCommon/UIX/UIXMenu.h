b2428d1c140be84c147fcc8e15f730c05ac17bb1
Mitchell Goodwin <mitch@gamesformay.com>
2026-01-17 23:52:58 +0000

    uix menu

---
+++ GameCommon/UIX/UIXMenu.h
@@ -0,0 +1,28 @@
+#ifndef UIX_PAGE_H
+#define UIX_PAGE_H
+
+#include <string>
+#include "UIX.h"
+
+class UIXPage : public UIXObject
+{
+public:
+	UIXPage( UIXObject* pxParent, uint32 uID, UIXRECT rect ) : UIXObject( pxParent, uID, rect ) {}
+
+	void		Initialise( const char* szTitle, BOOL bUseClipping );
+	
+	virtual UIXRECT		OnRender( InterfaceInstance* pInterface, UIXRECT rect );
+	virtual void		OnPostChildrenRender( InterfaceInstance* pInterface );
+
+private:
+	UIXRECT		mPageRenderRect;
+	BOOL		mbUseClipping = FALSE;
+	std::string		mTitle;
+
+};
+
+
+
+
+
+#endif
